bc8d700571cdb3a63c67d60b4409654e9f1a81d9
aturancetin <aturancetinn@gmail.com>
2026-05-15 15:49:04 +0200

    new changes

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -63,6 +63,7 @@ export interface Translations {
     emailPlaceholder: string;
     passengersLabel: string;
     passengersPlaceholder: string;
+    luggageLabel: string;
     flightLabel: string;
     flightPlaceholder: string;
     messageLabel: string;
@@ -77,6 +78,7 @@ export interface Translations {
     phoneField: string;
     emailField: string;
     passengersField: string;
+    luggageField: string;
     flightField: string;
     messageField: string;
     datePastError: string;
